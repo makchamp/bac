fb73304b82c58020e1b9c845653967f288289925
makchamp <makramadaime@hotmail.com>
2021-08-26 15:32:27 -0400

    Enable CORS policy this is required past socket-io 3

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const port = 3000;
 
 const app = require('express')();
 const httpServer = require('http').createServer(app);
-
-const io = require('socket.io')(httpServer);
+const options = { 
+	cors: {
+		origin: `http://${hostname}:5500`,
+		methods: ['GET', 'POST']
+	  }
+ };
+const io = require('socket.io')(httpServer, options);
 
 io.on('connect', socket => { 
 	console.log('connection established');
@@ -13,5 +18,3 @@ io.on('connect', socket => {
 httpServer.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
