100bc7c79dac175a983db05b285a1154ba5c988d
makchamp <makramadaime@hotmail.com>
2021-09-07 00:36:00 -0400

    Save and delete users in rooms Whenever a user joins a room, he is added to memory and then removed when disconnecting This is managed through a normal dict object

--- server/handlers/roomHandler.js
+++ server/handlers/roomHandler.js
@@ -1,11 +1,68 @@
+const rooms = {};
+
 module.exports = (io, socket) => {
   const joinRoom = (payload) => {
     const { userName, roomName } = payload;
+    const userID = socket.id;
     if (roomName) {
-      console.log(`${userName} is joining room ${roomName}`);
-      socket.join(payload.RoomName);
+      console.log(`User \'${userName}\' (${socket.id}) is joining Room \'${roomName}\'`);
+      socket.join(roomName);
+
+      // Save users in memory
+      if (rooms[roomName]) {
+        rooms[roomName].users[userID] = {
+          userName,
+          isHost: false
+        }
+      }
+      else {
+        rooms[roomName] = {
+          users: {}
+        }
+        rooms[roomName].users[userID] = {
+          userName,
+          isHost: true
+        }
+      }
+      notifyUserSetChanged(roomName);
+    }
+  }
+
+  const notifyUserSetChanged = (roomName) => {
+    // Update client with room info
+    const room = rooms[roomName];
+    if (!room) return;
+    const users = Object.keys(room.users).map((key) =>
+      ({ 
+        userName: room.users[key].userName,
+        isHost: room.users[key].isHost 
+      })
+    );
+
+    io.to(roomName).emit('room:userSetChanged', {
+      room: roomName,
+      users,
+    });
+  }
+
+
+  const leaveRoom = () => {
+    const roomsToRemove = [];
+    // Delete users from memory
+    for (const [key, value] of Object.entries(rooms)) {
+      delete value.users[socket.id];
+      notifyUserSetChanged(key);
+
+      // If the room is empty, also delete it
+      if (Object.keys(value.users).length === 0) {
+        roomsToRemove.push(key);
+      }
+    }
+    for (const room in roomsToRemove) {
+      delete rooms[roomsToRemove[room]];
     }
   }
 
   socket.on('room:join', joinRoom);
+  socket.on('disconnecting', leaveRoom)
 };
