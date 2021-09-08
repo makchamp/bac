8ec8994fd1564e90a37fbf9eebdfcefb9cf2ad73
makchamp <makramadaime@hotmail.com>
2021-09-08 19:48:21 -0400

    Controlled game settings panels

--- client/src/components/GameSettings.js
+++ client/src/components/GameSettings.js
@@ -6,9 +6,20 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Categories from './Categories';
+import { useState } from 'react';
 import Letters from './Letters';
 
 const GameSettings = () => {
+  const panels = {
+    categories: "categoriesPanel",
+    letters: "lettersPanel"
+  }
+  const [panelExpanded, setPanelExpanded] = useState(false);
+
+  const handlePanelExpansion = (panel) => (event, isExpanded) => {
+    setPanelExpanded(isExpanded ? panel : false);
+  }
+
   return (
     <Box
       sx={{
@@ -19,7 +30,10 @@ const GameSettings = () => {
       <Typography variant="h3" sx={{ m: 1 }}>
         Game Settings
       </Typography>
-      <Accordion defaultExpanded={true} >
+      <Accordion
+        expanded={panelExpanded === panels.categories}
+        onChange={handlePanelExpansion(panels.categories)}
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
         >
@@ -31,9 +45,12 @@ const GameSettings = () => {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion defaultExpanded={true} >
+      <Accordion
+        expanded={panelExpanded === panels.letters}
+        onChange={handlePanelExpansion(panels.letters)}
+      >
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}  
+          expandIcon={<ExpandMoreIcon />}
         >
           <Typography sx={{ width: '45%', flexShrink: 0 }}>Letters</Typography>
           <Typography sx={{ color: 'text.secondary' }}>Configures Letters To Play</Typography>
