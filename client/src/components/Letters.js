dca141c0eb02345b4c4feb8e857351b5df0e9936
makchamp <makramadaime@hotmail.com>
2021-09-08 19:37:58 -0400

    Letter rotation toggle Letter Rotation determines whether to play with a single or multiple letters per round

--- client/src/components/Letters.js
+++ client/src/components/Letters.js
@@ -1,5 +1,9 @@
 import ToggleButton from '@mui/material/ToggleButton';
 import Paper from '@mui/material/Paper';
+import Switch from '@mui/material/Switch';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import { useState } from 'react'
 
 const generateLetters = () => {
@@ -7,8 +11,8 @@ const generateLetters = () => {
   const alphabet = {};
   for (let i = 97; i <= 122; i++) {
     const letter = String.fromCharCode(i);
-    alphabet[letter] = { 
-      isActive: !lestCommon5.includes(letter), 
+    alphabet[letter] = {
+      isActive: !lestCommon5.includes(letter),
     };
   }
   return alphabet;
@@ -16,37 +20,57 @@ const generateLetters = () => {
 
 const Letters = () => {
   const [letters, setLetters] = useState(generateLetters);
+  // Letter Rotation determines whether to play with a single or multiple letters per round
+  const [letterRotation, setLetterRotation] = useState(false)
 
   const onLetterToggle = (letter) => {
     letters[letter].isActive = !letters[letter].isActive;
-    setLetters({...letters});
+    setLetters({ ...letters });
+  }
+
+  const onLetterRotationToggle = (event) => {
+    setLetterRotation(event.target.checked);
   }
 
   return (
-    <Paper
-      elevation={0}
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignContent: 'center',
-        p: 1,
-      }}
-    >
-      {Object.keys(letters).map((letter) => {
-        return (
-          <ToggleButton
-            key={letter}
-            value={letter}
-            selected={letters[letter].isActive}
-            onChange={() => onLetterToggle(letter)}
-            size="large"
-            sx={{ m: 0.5, fontSize: 20 }}
-          >
-            {letter}
-          </ToggleButton>)
-      })}
-    </Paper>
+    <Box>
+      <Paper
+        elevation={0}
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          alignContent: 'center',
+          p: 1,
+        }}
+      >
+        {Object.keys(letters).map((letter) => {
+          return (
+            <ToggleButton
+              key={letter}
+              value={letter}
+              selected={letters[letter].isActive}
+              onChange={() => onLetterToggle(letter)}
+              size="large"
+              sx={{ m: 0.5, fontSize: 20 }}
+            >
+              {letter}
+            </ToggleButton>)
+        })}
+      </Paper>
+      <Box sx={{ mt: 4 }}>
+        <Typography >Letter Rotation</Typography>
+        <Typography sx={{ color: 'text.secondary' }}>
+          Determine whether each category requires a different letter
+        </Typography>
+
+        <Stack direction="row" spacing={5} alignItems="center">
+          <Typography>Single Letter Per Round</Typography>
+          <Switch checked={letterRotation} onChange={onLetterRotationToggle} />
+          <Typography>Multiple Letters Per Round</Typography>
+        </Stack>
+      </Box>
+    </Box>
   );
 }
 
