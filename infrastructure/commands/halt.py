ec464e018127857512f6f2504ed0d3b9eaaebcd6
Averynder <averyndersingh@gmail.com>
2024-02-10 15:26:56 -0500

    Removing Old Jenkins Ties
    A few leftover lines from when Jenkins was in the process of being implemented were removed

--- infrastructure/commands/halt.py
+++ infrastructure/commands/halt.py
@@ -5,8 +5,8 @@ class Halt(ScriptCommand):
     def __init__(self):
         self.name="halt"
         self.help_message="used to stop the application on the specified machine"
-        self.choices=["prod", "jenkins", "vm", "all"]
-        self.argument = {"dest": "selection", "nargs": "+", "type": str, "metavar": "machine/s", "help": "prod|jenkins|vm|all"}
+        self.choices=["prod", "vm", "all"]
+        self.argument = {"dest": "selection", "nargs": "+", "type": str, "metavar": "machine/s", "help": "prod|vm|all"}
 
     def command(self, *args: str) -> None:
         print("Not Yet Implemented!")
