a88ffe38f072f98bbfd02c4d786719691883179f
Averynder <averyndersingh@gmail.com>
2024-02-13 18:42:57 -0500

    Updating Terminal Output
    Revamped Cleanup for Terminal Output since file method was discarded
    b's as well as excess ' characters and others have been removed
    Color has been added representing vagrant connection
    Final Lines may not be received due to current Method for submitting new commands

--- infrastructure/common.py
+++ infrastructure/common.py
@@ -15,15 +15,38 @@ TF_OUTPUT_OFFSET = 4
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-def fix_ansi(terminal_output: str) -> str:
-    initial = "^(b')"
-    new_line = r"\\r\\n'"
-    start_line = r"\\r"
-    esc = r"\\x1b"
-    new_esc = r"\033"
-    return_string = re.sub(f"{initial}|{new_line}|{start_line}", '', terminal_output)
-    return_string = re.sub(esc, new_esc, return_string)
-    return return_string
+def fix_ansi(terminal_output: bytes) -> str:
+    initial_one = "^(b')"
+    initial_two = "^(b\")"
+    end_one = "( ')$"
+    end_two = "( \")$"
+    esc_linux = "\\\\x1b"
+    start_line = "\\\\r"
+    new_line = "\\\\n"
+    cleaned_output = re.sub(f"{start_line}{new_line}'|{start_line}{new_line}", '\n', str(terminal_output))
+    cleaned_output = re.sub(f"{initial_one}|{initial_two}|{start_line}|{end_one}|{end_two}", "", cleaned_output)
+    cleaned_output = re.sub(f"{esc_linux}", "\\033", cleaned_output)
+
+    return cleaned_output
+
+def modified_line(current_line: str) -> str:
+    current_line = re.sub("vagrant@server", '\033[32mvagrant@server\033[0m', current_line)
+    initial_part, end_part = re.split(":.+$", current_line, 1)
+    mid_part = re.search(":.+$", current_line).group()[1:-1]
+    return f"{initial_part}:\033[34m{mid_part}\033[0m${end_part}"
+
+def fix_terminal_inputs(previous_line: str, current_line: str) -> list[str]:
+    if "vagrant@server" in current_line:
+        current_line = modified_line(current_line)
+    elif "vagrant@server" in previous_line:
+        previous_line = modified_line(previous_line)
+
+    currentline = ""
+    currentline = current_line.split('\n', 1)[0] or ''
+    nextline = current_line.split('\n', 1)[1] or ''
+    if previous_line:
+        currentline = f"{previous_line} {currentline}"
+    return [currentline, nextline]
 
 def format_tf_output(operation: str, output: tuple) -> str:
     match output[0]:
@@ -53,6 +76,8 @@ def ssh_commands(commands: list[str], time_limit: float) -> None:
     channel = ssh.invoke_shell()
     command_counter = 0
     time_counter = time_limit
+    current_line = ""
+    previous_line = ""
 
     while True:
         if channel.exit_status_ready():
@@ -60,7 +85,9 @@ def ssh_commands(commands: list[str], time_limit: float) -> None:
 
         if channel.recv_ready():
             output = channel.recv(1024)
-            print((fix_ansi(str(output))))
+            current_line = fix_ansi(output)
+            current_line, previous_line = fix_terminal_inputs(previous_line, current_line)
+            print(current_line)
             time_counter = time_limit
         else:
             time.sleep(1)
@@ -71,6 +98,7 @@ def ssh_commands(commands: list[str], time_limit: float) -> None:
                     command_counter += 1
                 time_counter = time_limit
 
+    print(previous_line)
     channel.close()
 
 def ssh_disconnect() -> None:
